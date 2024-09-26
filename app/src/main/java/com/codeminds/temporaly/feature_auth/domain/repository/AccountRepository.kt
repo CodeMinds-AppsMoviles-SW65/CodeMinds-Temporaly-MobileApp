2fb5fbb3835e9f7d7fd33e47292d0aa2b45fb23b
Alex Avila <alex.ramon2004@gmail.com>
2024-09-26 03:16:32 -0500

    fea(account): handle api error.

--- app/src/main/java/com/codeminds/temporaly/feature_auth/domain/repository/AccountRepository.kt
+++ app/src/main/java/com/codeminds/temporaly/feature_auth/domain/repository/AccountRepository.kt
@@ -4,6 +4,6 @@ import com.codeminds.temporaly.feature_auth.domain.model.Account
 
 interface AccountRepository {
 
-    fun getAccount(username: String, callback: (Account) -> Unit)
+    fun getAccount(username: String, callback: (Account?) -> Unit)
 
 }
