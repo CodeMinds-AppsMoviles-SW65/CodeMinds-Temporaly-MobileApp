30ca6b08a517d58d1f45de02bd2e509fa1e7a212
Alex Avila <alex.ramon2004@gmail.com>
2024-09-26 15:07:30 -0500

    feat(retro): added refit di module.

---
+++ app/src/main/java/com/codeminds/temporaly/core/di/RetrofitModule.kt
@@ -0,0 +1,26 @@
+package com.codeminds.temporaly.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    fun provideBaseUrl(): String = "https://temporally-api.ryzeon.me/api/v1/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
