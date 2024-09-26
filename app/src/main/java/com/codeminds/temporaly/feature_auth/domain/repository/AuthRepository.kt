65693bf65da30ce4ba6a846bd6799ddfacb9e7d0
Alex Avila <alex.ramon2004@gmail.com>
2024-09-26 02:58:14 -0500

    feat(auth): auth repo.

--- app/src/main/java/com/codeminds/temporaly/feature_auth/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/codeminds/temporaly/feature_auth/domain/repository/AuthRepository.kt
@@ -1,6 +1,14 @@
 package com.codeminds.temporaly.feature_auth.domain.repository
 
+import com.codeminds.temporaly.feature_auth.data.remote.dto.RefreshTokenResponseDto
+import com.codeminds.temporaly.feature_auth.data.remote.dto.SignInResponseDto
+import com.codeminds.temporaly.feature_auth.data.remote.dto.SignUpResponseDto
+
 interface AuthRepository {
 
+    fun signIn(usernameOrEmail: String, password: String, callback: (SignInResponseDto) -> Unit)
 
+    fun signUp(names: String, lastNames: String, email: String, password: String, roles: List<String>, callback: (SignUpResponseDto) -> Unit)
+
+    fun refreshToken(refreshToken: String, callback: (RefreshTokenResponseDto) -> Unit)
 }
