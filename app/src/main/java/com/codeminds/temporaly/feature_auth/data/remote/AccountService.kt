fa39cb28abc9ef77acf352c41a40a4f50053e8d9
Alex Avila <alex.ramon2004@gmail.com>
2024-09-26 03:05:37 -0500

    feat(account): account repository impl.

--- app/src/main/java/com/codeminds/temporaly/feature_auth/data/remote/AccountService.kt
+++ app/src/main/java/com/codeminds/temporaly/feature_auth/data/remote/AccountService.kt
@@ -1,11 +1,12 @@
 package com.codeminds.temporaly.feature_auth.data.remote
 
 import com.codeminds.temporaly.feature_auth.data.remote.dto.AccountDto
+import retrofit2.Call
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface AccountService {
 
     @POST("account/{username}")
-    fun getAccount(@Path("username") username: String): AccountDto
+    fun getAccount(@Path("username") username: String): Call<AccountDto>
 }
---
+++ app/src/main/java/com/codeminds/temporaly/feature_auth/data/repository/AccountRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package com.codeminds.temporaly.feature_auth.data.repository
+
+import com.codeminds.temporaly.feature_auth.data.remote.AccountService
+import com.codeminds.temporaly.feature_auth.data.remote.dto.AccountDto
+import com.codeminds.temporaly.feature_auth.data.remote.dto.toAccount
+import com.codeminds.temporaly.feature_auth.domain.model.Account
+import com.codeminds.temporaly.feature_auth.domain.repository.AccountRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class AccountRepositoryImpl(private val accountService: AccountService) : AccountRepository {
+
+    override fun getAccount(username: String, callback: (Account) -> Unit) {
+
+        accountService.getAccount(username).enqueue(object : Callback<AccountDto> {
+
+            override fun onResponse(call: Call<AccountDto>, response: Response<AccountDto>) {
+                if (response.isSuccessful) {
+                    val accountDto = response.body()
+                    if (accountDto != null) {
+                        callback(accountDto.toAccount())
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<AccountDto>, t: Throwable) {
+                // Handle error
+            }
+        })
+    }
+}
