9b895e12bbfd24154f6a7dd7768b670008f21425
Alex Avila <alex.ramon2004@gmail.com>
2024-09-26 02:54:23 -0500

    feat(account): added account repository.

---
+++ app/src/main/java/com/codeminds/temporaly/feature_auth/domain/repository/AccountRepository.kt
@@ -0,0 +1,9 @@
+package com.codeminds.temporaly.feature_auth.domain.repository
+
+import com.codeminds.temporaly.feature_auth.domain.model.Account
+
+interface AccountRepository {
+
+    fun getAccount(username: String, callback: (Account) -> Unit)
+
+}
